982f7854ad2801216794ad1c37922977e07c185e
Maciej Mieczakowski <maciej.mieczakowski@gmail.com>
2020-04-19 22:23:55 +0200

    Fixed a bug & workaround, all scanners has to use the same method nextLine, otherwise it sometimes jumps randomly leaving value unassigned as it doesn't wait for user input

--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -8,31 +8,23 @@ public class EmployeeTest {
         double totalWages = 0;
         Employee[] employees = new Employee[employeeMax];
 
-        //with one scanner only it does not work correctly, sometimes it jumps reading Line and goes to another stdin assigment
-        //for instance does not read Name and goes to Surname leaving name empty
-        Scanner scanner1 = new Scanner(System.in);
-        Scanner scanner2 = new Scanner(System.in);
-        Scanner scanner3 = new Scanner(System.in);
-        Scanner scanner4 = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < employees.length; i++) {
             System.out.print(" Name :");
-            String name = scanner1.nextLine();
+            String name = scanner.nextLine();
             System.out.print(" Surname: ");
-            String surname = scanner2.nextLine();
+            String surname = scanner.nextLine();
             System.out.print(" Salary: ");
-            String salary = scanner3.nextLine();
+            String salary = scanner.nextLine();
             employees[i] = new Employee(name, surname, Double.parseDouble(salary));
             System.out.print("Add another one? y/n : ");
-            String nextOne = scanner4.next();
+            String nextOne = scanner.nextLine();
             if (!nextOne.equalsIgnoreCase("y")) {
                 break;
             }
         }
-        scanner1.close();
-        scanner2.close();
-        scanner3.close();
-        scanner4.close();
+        scanner.close();
 
         for (Employee employee : employees) {
             if (employee != null) {
